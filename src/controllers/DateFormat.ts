5a0198f63222c817a86c4dfb0b0f4ade5704473b
Kevin Durbin <kevindurb@gmail.com>
2020-11-09 16:02:26 -0700

    add a clean date format controller

--- src/controllers/DateFormat.ts
+++ src/controllers/DateFormat.ts
@@ -3,11 +3,17 @@ import { Controller } from 'stimulus';
 import assert from 'assert';
 
 export default class extends Controller {
+  static targets = ['content'];
+
+  contentTarget?: HTMLElement;
+
   connect() {
     const value = this.data.get('value');
+    const content = this.contentTarget;
 
     assert(value, 'value to format as date not provided');
+    assert(content, 'no content target specified');
 
-    this.element.textContent = dateFns.format(dateFns.parseISO(value), 'PP');
+    content.textContent = dateFns.format(dateFns.parseISO(value), 'PP');
   }
 }
